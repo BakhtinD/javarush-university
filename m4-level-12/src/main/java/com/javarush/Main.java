1dba800aa34800b53e15202e85c0bfa4113cfd76
sproshchaev <sproshchaev@gmail.com>
2026-01-18 21:01:57 +0500

    init

--- m4-level-12/src/main/java/com/javarush/Main.java
+++ m4-level-12/src/main/java/com/javarush/Main.java
@@ -1,9 +1,27 @@
 package com.javarush;
 
+import com.javarush.entity.Employee;
+import com.javarush.entity.EmployeeTask;
+import com.javarush.entity.Product;
+import com.javarush.entity.User;
+import com.javarush.util.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // shutdown
+        HibernateUtil.shutdown();
     }
 
 }
